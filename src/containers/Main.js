e9c199d26fc21211b7ca5f2dfdbbdd290509b1e7
Bargavan R <bargavanroboticsengineer@gmail.com>
2025-03-31 22:25:40 +0530

    Github bug checking not fixed

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -19,7 +19,6 @@ import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
 import {useLocalStorage} from "../hooks/useLocalStorage";
-import GithubProfileCard from "../components/githubProfileCard/GithubProfileCard";
 import "./Main.scss";
 
 const Main = () => {
@@ -65,7 +64,6 @@ const Main = () => {
             <Twitter />
             <Podcast />
             <Profile />
-            <GithubProfileCard/>
             <Footer />
             <ScrollToTopButton />
           </>
