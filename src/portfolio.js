7a6b2391e80bbb486af4f49819baea39285b7228
Bargavan R <bargavanroboticsengineer@gmail.com>
2026-01-16 14:47:15 +0530

    test9

--- src/portfolio.js
+++ src/portfolio.js
@@ -424,7 +424,7 @@ const contactInfo = {
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
    //number: "+91-6374289565",
-  //email_address: "bargavanroboticsengineer@gmail.com",
+    email_address: "bargavanr01@gmail.com",
    display : true// Set false to hide this section, defaults to true
 };
 
